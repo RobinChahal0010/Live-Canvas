cf3a8f559b8d4b48a4907d2de57d8028b4790ad6
Robinpreet Singh Chahal <mantar.1911@gmail.com>
2026-08-07 15:24:32 +0530

    Multiple themes

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const themeBtn = document.getElementById("themeBtn");
+const themeButtons = document.querySelectorAll(".theme-color");
+const themeStatus = document.getElementById("themeStatus");
 
-themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+themeButtons.forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+        document.body.classList.remove(
+            "dark",
+            "purple",
+            "green",
+            "ocean"
+        );
+
+        const theme = btn.dataset.theme;
+
+        if(theme){
+            document.body.classList.add(theme);
+        }
+
+        themeStatus.textContent = theme || "Light";
+
+        localStorage.setItem("theme", theme);
+
+    });
+
 });
+
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme){
+
+    document.body.classList.add(savedTheme);
+
+    themeStatus.textContent = savedTheme;
+
+}
